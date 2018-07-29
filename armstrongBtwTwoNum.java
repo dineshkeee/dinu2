37ee0514fa3cc519eec0e8e9845a2a11a1269a5f
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-29 10:29:25 +0530

    Add files via upload

---
+++ armstrongBtwTwoNum.java
@@ -0,0 +1,22 @@
+package set2;
+import java.util.Scanner;
+public class armstrongBtwTwoNum {
+	public static void main(String args[]){
+		int a,b,c,d,i,l,sum=0;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		b=in.nextInt();
+		for(i=a;i<=b;i++,sum=0){
+		     l=Integer.toString(i).length();
+		    c=i;
+		while(c!=0){
+			d=c%10;
+			sum+=(Math.pow(d,l));
+			c/=10;
+		}
+		if(i==sum){
+			System.out.println(i);
+		}
+	 }
+   }
+}
