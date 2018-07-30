f520dbc3656466d805ca6bd30c78ea4228fc96ce
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-30 14:14:09 +0530

    Update spaceRemove.java

--- spaceRemove.java
+++ spaceRemove.java
@@ -2,10 +2,8 @@ package hunter;
 import java.util.*;
 public class spaceRemove {
 	public static void main(String args[]){
-		String a;
 		Scanner in=new Scanner(System.in);
-		a=in.nextLine();
-		String b=a.replaceAll("  "," ");
-		System.out.println(b);
+		String a=in.nextLine();
+		System.out.println(a.replaceAll("( )+"," "));
 	}
 }
