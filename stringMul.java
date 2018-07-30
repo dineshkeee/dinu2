aa19de840138125c02b01977658785cffdcbedad
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-30 15:05:19 +0530

    Add files via upload

---
+++ stringMul.java
@@ -0,0 +1,10 @@
+package hunter;
+import java.util.*;
+public class stringMul {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		String a=in.next();
+		String b=in.next();
+		System.out.println((String.valueOf(Integer.parseInt(a)*Integer.parseInt(b))));
+	}
+}
