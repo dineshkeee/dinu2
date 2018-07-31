c2c7ea2edeeefddfd7ac0fc8f3f3c32a1396d86c
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-31 20:45:00 +0530

    Add files via upload

---
+++ StringConcat.java
@@ -0,0 +1,10 @@
+package set3;
+import java.util.*;
+public class StringConcat {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		String a=in.next();
+		String b=in.next();
+		System.out.println(a+b);
+	}
+}
