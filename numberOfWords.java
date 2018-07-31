5896fc62e20915b77da96030c4184054d6ceb45d
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-31 14:47:47 +0530

    Add files via upload

---
+++ numberOfWords.java
@@ -0,0 +1,10 @@
+package setthree;
+import java.util.*;
+public class numberOfWords {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		String a=in.nextLine();
+		String arr[]=a.split(" ");
+		System.out.println(arr.length);
+	}
+}
