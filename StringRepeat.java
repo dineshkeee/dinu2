851beb710e9ee98e4dcde2fcc0cf5ee9ee727500
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-31 18:32:22 +0530

    Add files via upload

---
+++ StringRepeat.java
@@ -0,0 +1,12 @@
+package set3;
+import java.util.*;
+public class StringRepeat {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		String a=in.next();
+		int b=in.nextInt();
+		for(int i=1;i<=b;i++){
+			System.out.println(a);
+		}
+	}
+}
