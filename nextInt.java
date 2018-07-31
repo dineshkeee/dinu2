0ada7b28f55f24a53d0e2360a23a6c06681b8778
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-31 21:04:56 +0530

    Add files via upload

---
+++ nextInt.java
@@ -0,0 +1,9 @@
+package set3;
+import java.util.*;
+public class nextInt {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		System.out.println(a+1);
+	}
+}
